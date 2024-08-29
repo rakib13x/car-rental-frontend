e1e79a4b856aa3e7aba53cb04fbbc77f948bb1ae
rakib13x <rakib13x@gmail.com>
2024-08-29 12:08:23 +0600

    WhatWeAre Components added

---
+++ src/components/WhatWeAre.tsx
@@ -0,0 +1,28 @@
+import SectionTitle from "../Shared/SectionTitle/SectionTitle";
+import SectionWrapper from "../Shared/SectionWrapper/SectionWrapper";
+
+export default function WhatWeAre() {
+  return (
+    <SectionWrapper>
+      <SectionTitle
+        title={"What we are"}
+        subtitle={"We are writing a few about our history"}
+      />
+      <p className="text-center">
+        We offer you to choose from a wide variety of car classes new
+        high-quality vehicles meeting your needs and budget best. Renting a car
+        for your business enterprise or vacation, we have a wide range of
+        luxury, sports, and hybrid vehicles available to meet your every car
+        rental need. Take your pick of a BMW, Ferrari, Lamborghini, Aston
+        Martin, Tesla and more.
+      </p>
+      <img
+        className="mx-auto"
+        src="/car-about.jpg"
+        width={900}
+        height={350}
+        alt="About page"
+      />
+    </SectionWrapper>
+  );
+}
